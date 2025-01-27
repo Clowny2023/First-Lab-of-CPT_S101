59c0d60c946f3ac7463cffde8f0bd590fd27d34f
Alex Deng <137854871+Clowny2023@users.noreply.github.com>
2025-01-26 23:12:58 -0800

    Add files via upload

---
+++ 1月5号.py
@@ -0,0 +1,15 @@
+print(2**2**3*3)
+z=100/2.7/3.1
+y=(7*(14/3+2.3)**2)/(5-2**(3/2))**2
+print('z=',z,'y=',y)
+print('ha'*4)
+x=5
+print(str(x)+'$')
+cost=250
+print('That bike costs',str(cost)+'$.')
+#int(整数) float(小数)
+myList=[1,5,4,-2,0]
+print(len(myList))
+print(len('asuhvhh'))
+n=12345678909876543234567890987654
+print('The number',n,'contains',len(str(n)),'digits')
