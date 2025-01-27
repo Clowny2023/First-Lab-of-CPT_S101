18c65a927727fc54a8ef428aa21c8254e6bbf762
Alex Deng <137854871+Clowny2023@users.noreply.github.com>
2025-01-26 23:36:05 -0800

    first uploaded file

---
+++ teamwork.py
@@ -0,0 +1,14 @@
+##a=float(input('Enter how much :'))
+##q=a//25
+##d=a%25//10
+##n=a%25%10//5
+##p=a%25%10%5
+##print('Quarter(s)',str(q))
+##print('Dime()',str(d))
+##print('Nickel(s)',n)
+##print('Penny(s)',p)
+##print()
+x=0
+for i in range(1,81):
+    x+=2*i
+print(x)
